87e34b46c609cb196fe90e0c0f3b66df35157dc7
Alexander Mozhaev <rastamalik@gmail.com>
2017-12-18 11:05:09 +0300

    Added run_app.sh

--- run_app.sh
+++ run_app.sh
@@ -2,9 +2,9 @@
 sudo apt update
 sudo apt install -y ruby-full ruby-bundler build-essential
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927 -y 
-sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/ap$
+sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
 sudo apt update
-sudo pat install -y mongo-db
+sudo apt install -y mongo-db
 sudo systemctl start mongod
 sudo systemctl enable mongod
 git clone https://github.com/Otus-DevOps-2017-11/reddit.git
