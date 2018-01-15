f9b2d80c65aa1dc03c48b21525ed1ec7a5d84233
Alexander Mozhaev <rastamalik@gmail.com>
2018-01-15 14:11:32 +0300

    Modify stage2

---
+++ terraform/stage2/main.tf
@@ -0,0 +1,29 @@
+provider "google" {
+  version = "1.4.0"
+  project = "${var.project}"
+  region  = "${var.region}"
+}
+
+module "db" {
+  source          = "../modules/db"
+  public_key_path = "${var.public_key_path}"
+  zone            = "${var.zone}"
+  db_disk_image   = "${var.db_disk_image}"
+  private_key = "${var.private_key}"
+}
+
+
+module "app" {
+  source          = "../modules/app"
+  public_key_path = "${var.public_key_path}"
+  zone            = "${var.zone}"
+  app_disk_image  = "${var.app_disk_image}"
+ private_key = "${var.private_key}"
+}
+
+
+
+module "vpc" {
+  source        = "../modules/vpc"
+  source_ranges = ["0.0.0.0/0"]
+}
